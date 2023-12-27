4722ff09ecac22404f2956bf09643cbe851f66c5
jelly771001 <jelly771001@gmail.com>
2023-12-27 11:39:41 +0800

    updata Socket

--- src/Socket.jsx
+++ src/Socket.jsx
@@ -9,7 +9,7 @@ const Socket=()=>{
   const [userName, setUserName] = useState('')
   const [userNameInput, setUserNameInput] = useState('')
   useEffect(() => {
-    const socketInstance = io('http://localhost:3000')
+    const socketInstance = io('http://websocket-sample.azurewebsites.net')
     socketInstance.on('connect', () => {
       // setText(socketInstance.id)
     })
