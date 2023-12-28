0a012b48f09303981e582f290677298a466bf9de
jelly771001 <jelly771001@gmail.com>
2023-12-28 15:08:16 +0800

    updata 4

--- src/Socket.jsx
+++ src/Socket.jsx
@@ -1,73 +1,83 @@
 import { useState, useEffect } from 'react'
 import { HubConnectionBuilder } from '@microsoft/signalr'
 
-const Socket = () => {
+const SignalRComponent = () => {
   const [connection, setConnection] = useState(null)
-  const [currentMessage, setCurrentMessage] = useState('')
-  const [messages, setMessages] = useState([])
-  const [userName] = useState('Anonymous') // 添加用户名状态
+  const [userName, setUserName] = useState('')
+  const [content, setContent] = useState('')
+  const [lastEditTime, setLastEditTime] = useState('')
+  const [lastEditUser, setLastEditUser] = useState('')
+  const [onlineUsers, setOnlineUsers] = useState([])
 
-  useEffect(() => {
+useEffect(() => {
+  if (!connection) {
+    // 只在尚未建立连接时请求用户名
+    const name = window.prompt('请输入用户名')
+    setUserName(name || 'Anonymous')
+
+    // 创建SignalR连接
     const newConnection = new HubConnectionBuilder()
-      .withUrl('https://localhost:44339/SyscomHub1') // 替换为实际的Hub URL
+      .withUrl('https://localhost:5001/signalr') // 更新为实际的SignalR服务端点
       .withAutomaticReconnect()
       .build()
-
+    console.log(newConnection)
     setConnection(newConnection)
-  }, [])
+  }
+}, [connection]) 
 
   useEffect(() => {
     if (connection) {
+      // 启动连接
       connection
         .start()
         .then(() => {
-          console.log('Connected to the hub')
+          console.log('Connected!')
 
-          // 加入房间，传入用户名
-          connection.invoke('onJoinRoom', { Sid: connection.connectionId, Name: userName, ConnectedTime: new Date().toISOString() })
-
-          // 监听服务器发送的消息
+          // 监听事件
           connection.on('getOnlineList', (userList) => {
-            // 处理在线用户列表
-            console.log('Online Users:', userList)
+            setOnlineUsers(Object.values(userList))
           })
 
           connection.on('updateDocContent', (doc) => {
-            // 处理文档内容更新
-            console.log('Document Data:', doc)
-            setMessages((messages) => [...messages, doc.content])
+            setLastEditTime(doc.lastEditTime)
+            setLastEditUser(doc.lastEditName)
+            setContent(doc.content)
           })
-        })
-        .catch((err) => console.error('SignalR Connection Error: ', err))
-    }
-  }, [connection, userName]) // 依赖项中添加 userName
 
-  const sendMessage = async () => {
-    if (currentMessage.trim() !== '' && connection) {
-      try {
-        // 发送编辑文档的请求
-        await connection.invoke('EditDoc', { content: currentMessage, lastEditName: userName })
-        setCurrentMessage('')
-      } catch (err) {
-        console.error(err)
-      }
+          // 加入房间
+          connection.invoke('onJoinRoom', { Name: userName })
+        })
+        .catch((err) => console.error('Connection failed: ', err))
     }
+  }, [connection, userName])
+
+  const sendContent = () => {
+    connection.invoke('EditDoc', {
+      lastEditId: connection.connectionId,
+      lastEditName: userName,
+      content: content,
+    })
   }
 
   return (
     <div className="p-6">
-      <h1>Message App</h1>
-      <input type="text" value={currentMessage} onChange={(e) => setCurrentMessage(e.target.value)} placeholder="Type your message here" className="pl-2 mt-4 border-2 border-black rounded-md w-[200px]" />
-      <button onClick={sendMessage} className="border border-black px-2 py-1 ml-2 rounded-md hover:bg-black hover:text-white">
-        Send
-      </button>
+      <h1>Hello {userName}</h1>
       <ul>
-        {messages.map((msg, index) => (
-          <li key={index}>{msg}</li>
+        {onlineUsers.map((user, index) => (
+          <li key={index}>{user.Name}</li>
         ))}
       </ul>
+      <span>{lastEditTime}</span>
+      <span>{lastEditUser}</span>
+      <div className="edit-area">
+        <label>{content}</label>
+        <input type="text" value={content} onChange={(e) => setContent(e.target.value)} className="pl-2 mt-4 border-2 border-black rounded-md w-[200px]" />
+        <button type="button" onClick={sendContent} className="border border-black px-2 py-1 ml-2 rounded-md hover:bg-black hover:text-white">
+          Send
+        </button>
+      </div>
     </div>
   )
 }
 
-export default Socket
+export default SignalRComponent
