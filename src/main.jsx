1bab86b347339953704b926a9d621a28ff968fe0
jelly771001 <jelly771001@gmail.com>
2023-12-28 15:10:36 +0800

    updata socketNodeJs

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 
 import ReactDOM from 'react-dom/client'
-import App from './Socket.jsx'
+import App from './SocketNodeJs.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
