11d4e6ec0f18eeaadd964b1bf37a1ffde9c7360f
jelly771001 <jelly771001@gmail.com>
2024-01-04 10:26:33 +0800

    updata 0104/2

--- src/Socket.jsx
+++ src/Socket.jsx
@@ -82,80 +82,69 @@
 
 // export default SignalRComponent
 import { useState, useEffect } from 'react'
-import { HubConnectionBuilder, HttpTransportType } from '@microsoft/signalr'
+//import * as signalR from '@aspnet/signalr';
+import { HubConnectionBuilder, HttpTransportType, LogLevel } from '@aspnet/signalr'
+//import {HubConnectionBuilder,HttpTransportType} from '@aspnet/signalr';
 
 const SignalRComponent = () => {
   const [connection, setConnection] = useState(null)
-  const [userName, setUserName] = useState('jelly')
+  const [userName, setUserName] = useState('')
   const [content, setContent] = useState('')
   const [lastEditTime, setLastEditTime] = useState('')
   const [lastEditUser, setLastEditUser] = useState('')
   const [onlineUsers, setOnlineUsers] = useState([])
 
   useEffect(() => {
-    const configureSignalR = async () => {
-      try {
-        // 请求用户名
+    //初始化 SignalR 連接
+    const newConnection = new HubConnectionBuilder()
+      .configureLogging(LogLevel.Debug)
+      .withUrl(
+        'https://localhost:44341/signalr', // 更新為實際的 SignalR 服務端點
+        {
+          accessTokenFactory: () => {
+            if (typeof bearerToken !== 'undefined') {
+              return bearerToken.getToken()
+            }
+            return null // 如果未提供存取權杖，返回 null 或不提供此選項
+          },
+          skipNegotiation: true,
+          transport: HttpTransportType.WebSockets,
+        }
+      )
+      .build()
 
+    newConnection.on('getOnlineList', (userList) => {
+      setOnlineUsers(Object.values(userList))
+    })
 
-        // 创建 SignalR 连接
-        // const newConnection = new HubConnectionBuilderHubConnectionBuilder()
-        // .withUrl('https://localhost:44341/signalr')
-        // .build();
+    newConnection.on('updateDocContent', (doc) => {
+      setLastEditTime(doc.lastEditTime)
+      setLastEditUser(doc.lastEditName)
+      setContent(doc.content)
+    })
 
-        const newConnection = new HubConnectionBuilder()
-          .withUrl('https://localhost:7291/chathub', {
-            accessTokenFactory: () => {
-              if (typeof bearerToken !== 'undefined') {
-                return bearerToken.getToken()
-              }
-              return null // 如果未提供存取權杖，返回 null 或不提供此選項
-            },
-            skipNegotiation: true,
-            transport: HttpTransportType.WebSockets,
-          })
-          .withAutomaticReconnect()
-          .build()
-
-        console.log(newConnection)
-
-        // 设置连接状态监听
-        newConnection.onclose((error) => {
-          console.error('Connection closed:', error)
-        })
-
-        // 启动连接
-        await newConnection.start()
+    //啟動連接
+    newConnection
+      .start()
+      .then(() => {
         console.log('Connected!')
+        //請求用戶名
+        const name = window.prompt('請輸入用戶名')
+        setUserName(name || 'Anonymous')
 
-        // 监听事件
-        newConnection.on('getOnlineList', (userList) => {
-          setOnlineUsers(Object.values(userList))
-        })
-
-        newConnection.on('updateDocContent', (doc) => {
-          setLastEditTime(doc.lastEditTime)
-          setLastEditUser(doc.lastEditName)
-          setContent(doc.content)
-        })
-
-        // 加入房间
-        await newConnection.invoke('onJoinRoom', { Name: userName })
-
-        // 更新组件状态
-        setConnection(newConnection)
-      } catch (error) {
+        //加入房間
+        newConnection.invoke('onJoinRoom', { Name: userName })
+      })
+      .catch((error) => {
         console.error('Connection failed:', error)
-      }
-    }
+      })
 
-    // 只在尚未建立连接时配置 SignalR
-    if (!connection) {
-      configureSignalR()
-    }
-  }, [connection, userName])
+    //更新狀態
+    setConnection(newConnection)
+  }, []) // 注意：這裡的空 dependency array 確保只在組件掛載時初始化一次
 
   const sendContent = () => {
+    //發送內容
     connection.invoke('EditDoc', {
       lastEditId: connection.connectionId,
       lastEditName: userName,
